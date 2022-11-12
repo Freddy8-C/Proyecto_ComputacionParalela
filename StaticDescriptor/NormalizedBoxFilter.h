2364d98ff076287deae40328aebf3b3727927e68
Freddy <fc.freddy00@gmail.com>
2022-11-12 18:43:07 -0500

    Fin

--- StaticDescriptor/NormalizedBoxFilter.h
+++
@@ -1,4 +0,0 @@
-#ifndef NORMALIZEDBOXFILTER_H
-#define NORMALIZEDBOXFILTER_H
-int suma(int a, int b);
-#endif // NORMALIZEDBOXFILTER_H
--- StaticDescriptor/Smoothing.h
+++
@@ -1,4 +0,0 @@
-#ifndef SMOOTHING_H
-#define SMOOTHING_H
-
-#endif // SMOOTHING_H
